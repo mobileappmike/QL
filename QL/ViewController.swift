48ee90bed9308b1e742dffc1e3ce994bff929c97
michaelmiles1 <michael.gage.miles@gmail.com>
2019-09-26 15:48:51 -0700

    Initial Commit

---
+++ QL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QL
+//
+//  Created by Michael Miles on 9/26/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
